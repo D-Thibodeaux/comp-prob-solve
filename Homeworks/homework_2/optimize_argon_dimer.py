d66f846f9a8d52a2e7949210c91720861f0d425a
Douglas Thibodeaux <dthibodeaux234@gmail.com>
2024-09-25 14:44:46 -0500

    Uploading for homework 2 and some reorganization

--- Homeworks/homework_2/optimize_argon_dimer.py
+++ Homeworks/homework_2/optimize_argon_dimer.py
@@ -7,6 +7,17 @@ from homework_1_2 import compute_bond_length, compute_bond_angle
 
 #define Lennard Jones potential with preset epsilon and sigma values
 def lennard_jones(r, epsilon=0.01, sigma=3.4):
+    """
+    given a pair separation distance and the appropriate constants, calculate the energy of a pair of atoms using the Lennard Jones potential.
+
+    parameters:
+    r (float): pair separtion distance in angstroms
+    epsilon (float) : a value corresponding to the epsilon value in the lennard jones 12 6 potential. has units of eV
+    sigma (float) :  a value corresponding to the sigma value in the lennard jones 12 6 potential. has units of angstroms
+
+    returns: 
+    potential energy of the system in eV
+    """
     V_lj = 4 * epsilon * ((sigma / r) ** 12 - (sigma / r) ** 6)
     return V_lj
 
@@ -15,9 +26,13 @@ optimum_length = minimize(fun=lennard_jones, x0=4, method="Nelder-Mead", tol=1e-
 print(optimum_length)
 
 #build an extxyz file to view the optimum structure
-pos = np.array([[0, 0, 0], [0, optimum_length, 0]])
-Ar = Atoms('Ar2', pos)
-write("dimer.extxyz", Ar, format = 'extxyz')
+pos_set = np.array([[0, 0, 0], [0, optimum_length, 0]])
+#make xyz file to show optimum structure
+file = open('dimer.xyz', 'wt')
+file.write(str(len(pos_set)) + "\n")
+file.write("Argon dimer file\n")
+for pos in pos_set:
+    file.write(f"Ar   {pos[0]}   {pos[1]}   {pos[2]}\n")
 
 #build a set of radius values
 r_values = np.linspace(3, 6, 300)
--- Homeworks/homework_2/optimize_argon_trimer.py
+++ Homeworks/homework_2/optimize_argon_trimer.py
@@ -1,22 +1,39 @@
 import numpy as np
 from scipy.optimize import minimize
-from ase.io import write
-from ase import Atoms
 import matplotlib.pyplot as plt
 from homework_1_2 import compute_bond_length, compute_bond_angle
 
 #define a function to determine the the energy of the three Argon particle system
 def lennard_jones_trimer (parameters, epsilon=0.01, sigma=3.4):
+    """
+    Given a set of points, calculate the energy of an Argon trimer using a lennard jones potential.
+    this function assumes the following:
+    one particle (particle 1) is at the position [0,0,0]
+    one particle (particle 2) is at [x2,0,0] where x2 is a variable that corresponds to the distance between particles 1 and 2
+    one particle (particle 3) is at [x3,y3,0] where x3 and y3 correspond to any set of x and y positions
+
+    Parameters:
+    parameters (3 item array) : an array containg the values x2,x3 and y3 in that order. each of these values may be a float or an integer
+    epsilon (float) : a value corresponding to the epsilon value in the lennard jones 12 6 potential. has units of eV
+    sigma (float) :  a value corresponding to the sigma value in the lennard jones 12 6 potential. has units of angstroms
+
+    returns: 
+    potential energy of the system in eV
+    """
+    #unpack parameters
     x2, x3, y3 = parameters
 
+    #defime positions
     pos_1 = np.array([0,0,0])
     pos_2 = np.array([x2,0,0])
     pos_3 = np.array([x3,y3,0])
 
+    #compute distances from positions
     r12 = compute_bond_length(pos_1, pos_2)
     r13 = compute_bond_length(pos_1, pos_3)
     r23 = compute_bond_length(pos_2, pos_3)
 
+    #calculate energ
     V_lj = 0
     for r in [r12, r13, r23]:
         V_lj += 4 * epsilon * ((sigma / r) ** 12 - (sigma / r) ** 6)
@@ -39,8 +56,11 @@ print('θ123 = ', compute_bond_angle(pos_set[0], pos_set[1], pos_set[2]))
 print('θ231 = ', compute_bond_angle(pos_set[1], pos_set[2], pos_set[0]))
 print('θ312 = ', compute_bond_angle(pos_set[2], pos_set[0], pos_set[1]), '\n')
 
-print("The bond distances are all approximately equal and all bond angles are 60 degrees. This means that the Argons are arranged in an equilateral triangle.")
+print("The bond distances are all equal and all bond angles are 60 degrees. This means that the Argons are arranged in an equilateral triangle.")
 
-#make extxyz file to show optimum structure
-Ar = Atoms('Ar3', pos_set)
-write("trimer.extxyz", Ar, format = 'extxyz')
+#make xyz file to show optimum structure
+file = open('trimer.xyz', 'wt')
+file.write(str(len(pos_set)) + "\n")
+file.write("Argon trimer file\n")
+for pos in pos_set:
+    file.write(f"Ar   {pos[0]}   {pos[1]}   {pos[2]}\n")
